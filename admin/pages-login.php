651f2b4e9e5b5eb93397e9c0c656ef48433824f1
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-01-11 00:53:31 +0100

    Worked on the meta tag in admin dashboard

--- admin/pages-login.php
+++ admin/pages-login.php
@@ -14,10 +14,10 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>SimpleAdmin - Responsive Admin Dashboard Template</title>
+        <title>Admin - Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-        <meta content="Coderthemes" name="author" />
+        <meta content="A fully featured admin dashboard for Gentlecypto enterprise" name="description" />
+        <meta content="World Brain Technology" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <!-- App favicon -->
