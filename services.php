f8b04f2ad98ff766671f4d613d13a1785973c33a
Godson Pius <godsonazubuike15@gmail.com>
2022-09-05 04:16:42 +0100

    Update service

--- services.php
+++ services.php
@@ -38,24 +38,213 @@
 
 	} else if ($_GET['service'] === 'ccc') {
 		$title = "Crypto currency consultancy";
-		$description = "We are open to consultations on issues revolving around crypto currency and any of the digital asset projects. It is entirely free for our students but comes with a fee for those who are none students. <br /> <br />  <h4>What can I expect</h4> <br />
-		1. A brief conversation about how Gentlecrypto Enterprise can help you with any of your issues.<br />
-		
-		2. Live product demonstration with insight into how to get the most benefit from Gentlecrypto Enterprise<br />
-		
-		3. Any questions you have about Gentlecrypto Enterprise answered<br />
-		
-		3. A thorough counselling on investment decisions and choices <br />
-		
-		4. No commitment required
-		Our demonstrations are 100% live.<br />
-		Our demonstrations are 100% live so – you get a product overview that suits your needs and we can answer any questions you have during the demonstration. <br /> <b>Connect with our chief consultant</b> <a class='btn btn-primary' href='https://api.whatsapp.com/send?phone=09034931935'>click here</a>"; 
+		$description = "<h6>Blockchain &amp; Digital Asset Consulting
+		Services</h6>
+		Leverage the expertise and deep Crypto background of the best minds at Gentlecrypto
+		Consultancy to solve your hardest crypto and blockchain challenges in order to acquire the
+		capabilities needed to excel in this industry.<br /><br />
 
-	} else if ($_GET['service'] === 'ccc') {
-		$title = "Crypto currency consultancy";
-		$description = "We are open to consultations on issues revolving around crypto currency and any of the digital asset projects. It is entirely free for our students but comes with a fee for those who are none students. You have a question regarding any aspect of digital asset investing or personal finance, then do well to <a href='courses.php'>contact us!</a>";
+		We are dedicated to helping our clients navigate the ever-changing world crypto and
+		Blockchain technology on their initial coin offerings (ICO), Non fungible token (NFTs) launch,
+		project launch ETC.<br /><br />
 
-	} else if ($_GET['service'] === 'cct') {
+		Our <strong>comprehensive consulting service</strong> empowers crypto and blockchain organizations
+		to achieve lasting gains in performance and innovate more sustainably in the world of
+		digital asset and blockchain space. We offer professional advice on the planning, formation,
+		incorporation and management of crypto related businesses.<br /><br />
+
+		We aide aspiring crypto currency entrepreneurs in their journey into the Blockchain. Our
+		clients will leverage on our in-depth understanding of the complexities of assessing the
+		market globally, as well as the risks associated with it cause of volatility and regulations.
+		Working with our team ensures you will properly analyze the risk and can limit any
+		potential regulatory issues anywhere around the world. Our goal is to ensure that our
+		clients comply with the unique legal and regulatory issues specific to their project.<br /><br />
+
+		Our Consultants are dedicated to helping our clients navigate the ever-changing world of
+		initial coin offerings. We offer professional advice on the planning, formation,
+		incorporation, and management of ICO-related businesses, as well as guidance in handling
+		transactions.<br /><br />
+
+		<h6>Our Services</h6> 
+		Whether you are looking to make a foray into the blockchain space, select a protocol for
+		development, launch a new decentralized product/service or just strengthen your presence
+		in the market, partner with us for guidance.<br /><br />
+
+		Our blockchain consulting services focused on areas like <strong>DeFi, NFTs, metaverse,
+		Blockchain, decentralized gaming, IoTs, web3 and Blockchain development</strong> will help
+		your business expand its core competencies and implement technologies better.<br /><br />
+
+		<ul>
+			<li>Market Research and Analysis</li>
+			<li>ICO listing services</li>
+			<li>ICO marketing services</li>
+			<li>White paper writing</li>
+			<li>Financial Analysis</li>
+			<li>Workshops and Trainings</li>
+			<li>Business &amp; Organizational Advisory Services</li>
+		</ul> <br />
+
+		<h6>Market Research &amp; Analysis</h6> 
+		Among the services we offer is financial analysis and market research. Market research is
+		the focused study of different marketing strategies and the effects they have on different
+		segments of the population.<br /><br />
+
+		A market researcher may decide to populate a focus group with people of a particular
+		demographic and then ask them questions about their responses to different products or
+		advertisements. The market researcher then uses this feedback to shape future marketing
+		campaigns, or even change the product itself.<br /><br />
+
+		One of the most important demographic distinctions our market researchers make concerns
+		wealth and pricing. People with different amounts of money favor products in different price
+		ranges, and so setting your price to target the investors or customers you’re looking for is
+		absolutely essential.<br /><br />
+
+		The whole point of market research is to construct a detailed marketing plan, which is itself
+		a tool to attract investors.
+
+		<a href='./contacts.php' class='btn btn-primary'>Work with us</a> <br /><br />
+
+		<h6>ICO listing services</h6>
+
+		Get no hassles listing your crypto and tokens on Top Exchanges &amp; trading platforms across
+		the globe.<br /><br />
+
+		Gentlecrypto provides Crypto Listing Services to crypto tokens and coins to gain more
+		liquidity exposure for your digital asset. If you’ve recently created a token or you intend to
+		create one, and are seeking a trustworthy company to market it for you or help you list
+		your projects on exchanges globally, then you are at the right place. <br /><br />
+
+		Listing on major exchanges and data aggregators’ websites is a very crucial step for newly
+		launched digital assets. Listing helps in attracting a vibrant community of investors who are
+		eager to start your journey with you and capitalize on the opportunities.<br /><br />
+
+		We are one stop solution for all the services relating to the development and launch of
+		crypto assets. <br /><br />
+
+		If you want to attract more investors, you have to increase liquidity and listing on major
+		exchanges help in that. It is time-consuming process which is complicated, tough and
+		requires a lot of technical knowledge and necessary connections. However, our team will
+		take care of all the technicalities of the procedure and complete the listings without any
+		hassle. <br /><br />
+
+		Every platform has different requirements and we take care of that with thorough
+		preparation and documentation.
+		Gentlecrypto consultancy has partnered with top crypto asset listing platforms where
+		investors rely and check for new coins. We customize the service according to the best
+		suited platforms for your digital asset. <br /><br />
+
+		Our experienced crypto currency Consultants will ensure that your coin or token is
+		efficiently listed on major exchanges depending on our agreement.
+		
+		<a href='./contacts.php' class='btn btn-primary'>Work with us</a> <br /><br />
+
+		<h6>ICO Marketing Services</h6>
+		Consulting subsidiary also offers ICO marketing services, including: 
+		
+		<ul>
+			<li><b style='color: white'>Digital Analysis</b> – We do research based on SEO, keywords, and the competitive
+			landscape.</li>
+			<li><b style='color: white'>Website optimization</b> - we offer optimization of your website.</li>
+			<li><b style='color: white'>Content Creation</b> - We create eye-catching and consumer-friendly content.</li>
+			<li><b style='color: white'>Website Development</b> - We offer exceptional development</li>
+			<li><b>Marketing Advisory roles.</b></li>
+			<li><b>Marketing and sales copy</b></li>
+		</ul>
+
+		Our team at <b>Gentlecrypto Consultancy</b> is always on standby to offer observations
+		regarding new projects and help determine the most effective marketing strategy. The
+		observation contributes to outstanding results in fundraising efforts and traffic generation.
+		Our team can also help develop content to leverage any social media, website, and P.R.
+		opportunities you may have. <br /><br />
+
+		After creating a good ICO white paper and ICO website, it’s time to market it. The best
+		means of marketing an ICO is through search engine optimization. We recommend that you
+		get the services of a good ICO consulting service for the most up-to-date marketing
+		strategies,that is where we come in. <br /><br />
+
+		These are a few ICO marketing tactics we’ve had success with:
+		<ul>
+			<li><b style='color: white'>Guest posting</b> – The company drafts guest posts on websites with high traffic.</li>
+			<li><b style='color: white'>Content marketing</b> – Reach out to your target audience through informative or
+			entertaining content.</li>
+			<li><b style='color: white'>Sponsored content</b> – Develop sponsored content on websites with high traffic.</li>
+			<li><b style='color: white'>Sponsored ads</b> – Place ads on websites with high traffic.</li>
+		</ul>
+
+		<a href='./contacts.php' class='btn btn-primary'>Work with us</a> <br /><br />
+
+		<h6>Whitepaper Writing</h6>
+		An ICO white paper is an all-inclusive document where you describe the type of project you
+		want to start, your target market, and the tactics you plan to use. The white paper can be
+		likened to the form S1 that companies that want to be listed publicly submit to the SEC.<br /><br />
+
+		A great white paper highlights the gap in the market or the problem your team intends to
+		solve, demonstrating deep insight into the market and giving you an opportunity to talk
+		about past successes. <br /><br />
+
+		At Gentlecrypto Consultancy, we will help you write and revise the perfect white paper. We
+		will also provide you with links to a service page anchor. We’ll also help you draft
+		professional prospectuses and information memoranda for token offerings and provide
+		skillfully drafted private placement documents. <br /><br />
+
+		Here are the key components of a compelling white paper along with notes on what you
+		ought to do in each section:
+
+		<ul>
+			<li><b style='color: white'>The Abstract</b>: summarize the project</li>
+			<li><b style='color: white'>The Problem</b>: describe the problem you’re trying to solve</li>
+			<li><b style='color: white'>The Solution</b>: describe your solution to the problem</li>
+			<li><b style='color: white'>The Roadmap</b>: explain how your business will develop over time</li>
+			<li><b style='color: white'>Funds Estimate</b>: list your expenses and estimate how much money you need</li>
+			<li><b style='color: white'>Market Analysis</b>: demonstrate your knowledge of the market and highlight the utility
+				of your idea</li>
+			<li><b style='color: white'>Go-to-Market Strategy</b>: explain how you’re going to get your idea on the market</li>
+			<li><b style='color: white'>Team Summary</b>: describe your team members, their experiences, and their roles
+				The success of an ICO is dependent on many factors, including but not limited to excellent
+				white paper services. Gentlecrypto Consultancy can help make sure your white paper gets
+				noticed.</li>
+		</ul>
+
+		<a href='./contacts.php' class='btn btn-primary'>Work with us</a> <br /><br />
+
+		<h6>Workshops &amp; Trainings</h6>
+		We organizes interactive, practical, applicable and valuable professional, public, in-house and
+		online crypto, web3, NFT, Metaverse and blockchain and financial training courses for business
+		executives and banking and finance professionals all over the world. <br /><br />
+
+		Our events cover a wide range of topics from Asset and Liability Management, Corporate
+		Finance, Personal Finance, Risk Management, Metaverse, Crypto Markets, Blockchain
+		Adoption, Investment Management, Trading, Marketing, Business Management, Loan
+		Management, Crypto Utility, Web3 and more.
+
+		<b>What do you get out of our Trainings &amp; workshops?</b>
+		<ul>
+			<li><b style='color: white'>Practical</b>: You learn skills that are relevant to mastering your job.</li>
+			<li><b style='color: white'>Applicable</b>: The skills you learn on our courses can be applied to your job.</li>
+			<li><b style='color: white'>Valuable</b>: You receive a much greater return for your investment.</li>
+			<li>Specially designed training by industry active experts for industry practitioners</li>
+			<li>Our trainers have a passion for their chosen topics</li>
+			<li>Not just a lecture but an interactive dialogue between you, the trainer and your classmates</li>
+			<li>You gain knowledge and skills that make you valuable to your company and your career</li>
+		</ul> <br /><br />
+
+		<h6>Business &amp; Organizational Advisory Services</h6>
+
+		Access our full service of resources for blockchain companies and businesses looking to
+		incorporate blockchain and crypto into their current business model. <br /><br />
+
+		<h6>Our Consultants</h6>
+		Igwenagu Emmanuel <br />
+		CEO Gentlecrypto Enterprises <br /><br />
+
+		<h6>Let’s Discuss Your Project Idea</h6>
+		Integrate your ideas with our Expertise to drive your project to the moon
+		Talk with our Consultant
+
+		<a href='./contacts.php' class='btn btn-primary'>Work with us</a> <br /><br />
+		"; 
+
+	}  else if ($_GET['service'] === 'cct') {
 		// $title = "Crypto currency trading";
 		$title = "Coming Soon";
 		// $description = "we can help you and trade profitable on the crypto market all you need to do is get your funds to us and we begin";
